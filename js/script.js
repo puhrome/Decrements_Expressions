0a799fe0658ddbace9e7622a988abf35ce0599c5
puhrome <phirom.yim@fullsail.edu>
2014-05-09 16:40:25 -0400

    Decrements Expression
    Using WebStorm to explain different types of decrements

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/**
+ * Decrements Created by Phirom Yim on 5/9/14.
+ */
+//Decrements -- myVariable
+
+/*--myVariable;
+myVariable = myVariable - 1;
+myVariable--;
+*/
+console.log("myVariable--");//print myVariable String to console
+
+myNumber = (prompt("Enter Your First Number"));//prompt for user input
+myVar = myNumber-- + 20;//declare and define variable in decrement
+console.log(myNumber);//print to console
+
+console.log("myVariable = myVariable - 1");//print string to console
+
+myNum = (prompt("Enter Your Second Number"));//prompt for user input
+myNum = myNum - 1;//declare and define variable in decrement
+console.log(myNum);//print to console
+
+console.log("--myVariable");//print string to console
+
+myNum3 = (prompt("Enter Your Third Number"));//prompt for user input
+myVar3 = --myNum3 + 30;//declare and define variable in decrement
+console.log(myNum);//print to console
+
